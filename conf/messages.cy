1305931c3dd6c9321e3117108f242d1e5c4c8ae1
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2026-07-14 13:59:04 +0000

    'Creating new service sdec-internal-frontend-alpha-test'

---
+++ conf/messages.cy
@@ -0,0 +1,2 @@
+service.name = sdec-internal-frontend-alpha-test
+service.text = Dyma'ch gwasanaeth newydd
---
+++ project/AppDependencies.scala
@@ -0,0 +1,21 @@
+import sbt.Keys.libraryDependencies
+import sbt._
+
+object AppDependencies {
+
+  private val bootstrapVersion = "10.8.0"
+  
+
+  val compile = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "13.9.0"
+  )
+
+  val test = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
+    
+    "org.jsoup"               %  "jsoup"                      % "1.13.1"            % Test,
+  )
+
+  val it = Seq.empty
+}
