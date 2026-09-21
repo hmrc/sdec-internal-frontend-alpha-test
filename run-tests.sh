391544b7e30dfeb3efc82785b25bbaa0fee44984
amityadav-crypto <320564726+amityadav-crypto@users.noreply.github.com>
2026-09-21 16:27:28 +0100

    SDEC-1044 Updated run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -5,10 +5,4 @@ ENVIRONMENT=$2
 HEADLESSFLAG=$3
 PREVIOUSVERSIONFLAG=$4
 
-if [ -z "$BROWSER_TYPE" ]; then
-    echo "BROWSER_TYPE value not set, defaulting to $DEFAULT_BROWSER..."
-    echo ""
-fi
-
-
 sbt scalafmtAll scalafmtCheckAll scalafmtSbtCheck clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dbrowser.option.headless="${HEADLESSFLAG:=true}" -Dbrowser.usePreviousVersion="${PREVIOUSVERSIONFLAG:=true}" "testOnly uk.gov.hmrc.ui.specs.* -- -n AcceptanceTests" testReport
