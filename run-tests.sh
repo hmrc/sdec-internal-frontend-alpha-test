7f32d810c02f7b434ac1ee8ccd5ef519970ddf8f
Amit Yadav <320564726+amityadav-crypto@users.noreply.github.com>
2026-09-23 10:51:55 +0100

    Apply suggestion from @gianick
    Co-authored-by: Nikolas <gianick@users.noreply.github.com>

--- run-tests.sh
+++ run-tests.sh
@@ -5,4 +5,4 @@ ENVIRONMENT=$2
 HEADLESSFLAG=$3
 PREVIOUSVERSIONFLAG=$4
 
-sbt scalafmtCheckAll scalafmtSbtCheck clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dbrowser.option.headless="${HEADLESSFLAG:=true}" -Dbrowser.usePreviousVersion="${PREVIOUSVERSIONFLAG:=true}" "testOnly uk.gov.hmrc.ui.specs.* -- -n AcceptanceTests" testReport
+sbt scalafmtCheckAll scalafmtSbtCheck clean compile -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dbrowser.option.headless="${HEADLESSFLAG:=true}" -Dbrowser.usePreviousVersion="${PREVIOUSVERSIONFLAG:=true}" "testOnly uk.gov.hmrc.ui.specs.* -- -n AcceptanceTests" testReport
